5613b8fec57d5480fd5b61ae91d0b5d344347ba4
VigilV11 <vigilv@pm.me>
2021-01-21 20:06:35 +0530

    Added ability to add tasks. Added task button icons.

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const timerBtn = document.querySelector('#timer-btn');
 const timeDisplay = document.querySelector('#time-display');
 const resetBtn = document.querySelector('#reset-btn');
+const addTask = document.querySelector('.add-task-btn');
+const taskInput = document.querySelector('.task-input');
+const taskListDiv = document.querySelector('#task-list-div');
 
 let timerOn = false; // Is timer ON (true) or OFF (false)?
 let totalTime = 0; // total time elapsed in milliseconds
 let timerStartTime; // timer start timestamp
 let intervalTimer;
+const taskList = [];
 
 //========== DISPLAY TIME ELAPSED ==========\\
 // Convert to hours mins:secs
 const displayTime = function(timeElapsed) {
   // timeElapsed is in milliseconds
 
+  const ms = Math.round(timeElapsed / 100);
   const timeInSecs = Math.round(timeElapsed / 1000);
   const secs = timeInSecs % 60;
   const timeInMins = (timeInSecs - secs) / 60;
@@ -23,13 +28,13 @@ const displayTime = function(timeElapsed) {
     timeDisplay.textContent = `${hrs}h ${String(mins).padStart(
       2,
       '0'
-    )}:${String(secs).padStart(2, '0')}`;
+    )}m ${String(secs).padStart(2, '0')}s`;
   } else {
     // if no hours, display mins:secs
     mins = timeInMins;
-    timeDisplay.textContent = `${String(mins).padStart(2, '0')}:${String(
+    timeDisplay.textContent = `${String(mins).padStart(2, '0')}m ${String(
       secs
-    ).padStart(2, '0')}`;
+    ).padStart(2, '0')}s`;
   }
 };
 
@@ -80,3 +85,44 @@ timerBtn.addEventListener('click', () => {
 
 // Reset the timer
 resetBtn.addEventListener('click', timerReset);
+
+addTask.addEventListener('click', () => {
+  if (taskInput.value !== '') {
+    console.log(taskInput.value);
+    const html = `
+    <div>
+    <span>${taskInput.value}</span>  &nbsp;&nbsp;&nbsp;&nbsp;
+    <span id="time-display">00m 00s</span>
+    
+
+
+    <i class="fas fa-play-circle"></i> &nbsp;
+    <i class="fas fa-pause"></i> &nbsp;
+    <i class="fas fa-sync-alt"></i> &nbsp;
+    <i class="fas fa-trash"></i> &nbsp;
+    <i class="fas fa-check-circle"></i> &nbsp;&nbsp;
+
+    <br />
+    <br />
+    </div>
+    
+
+    <!--
+    <button class="btn"><i class="fa fa-trash"></i></button>
+    <button class="btn"><i class="fas fa-play-circle"></i></button>
+    <button id="timer-btn" class="btn-off-state">Start</button>
+    <button id="reset-btn" class="btn-off-state">Reset</button>
+    -->
+    `;
+    taskListDiv.insertAdjacentHTML('beforeend', html);
+  }
+
+  taskInput.value = '';
+  taskInput.focus();
+  taskInput.select();
+});
+
+// Adding event delegation to handle buttons related to individual tasks
+taskListDiv.addEventListener('click', function(e) {
+  // some logic here
+});
